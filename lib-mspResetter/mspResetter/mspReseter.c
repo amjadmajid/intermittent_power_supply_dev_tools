2be3e988cd11660ee99b0681ce4986b9f84251b0
painter105 <patryk_105@hotmail.com>
2018-05-30 18:13:16 +0200

    Added offline-time (also using powerPatterns) before reset

--- lib-mspResetter/mspResetter/mspReseter.c
+++ lib-mspResetter/mspResetter/mspReseter.c
@@ -1,5 +1,5 @@
 #include <msp430.h> 
-#include <mspDebugger.h>
+#include "mspDebugger.h"
 
 #define __nv  __attribute__((section(".nv_vars")))
 
@@ -18,7 +18,7 @@
 #endif
 
 __nv unsigned int __noiseSel = 0;
-
+__nv unsigned int rstInterval = 0;
 __nv unsigned __reset = 1;
 
 void mr_reseter()
@@ -54,6 +54,9 @@ void mr_auto_reseter(unsigned int interval)
 
 void mr_auto_rand_reseter(unsigned int interval)
 {
+   rstInterval = interval;
+
+
    if(__noiseSel >= 200)
    {
        __noiseSel=0;
@@ -61,8 +64,9 @@ void mr_auto_rand_reseter(unsigned int interval)
 
 
   TA0CCTL0 = CCIE;                          // TACCR0 interrupt enabled
-  TA0CCR0 =interval+__noise[__noiseSel];
+  TA0CCR0 =rstInterval+__noise[__noiseSel]; // comment: noise is amplified as well by divider
   TA0CTL = TASSEL__SMCLK | MC__UP;   // SMCLK, continuous mode
+  //TA0CTL = TASSEL__SMCLK | MC__UP | ID_3;   // SMCLK, continuous mode, divider 8
 
   __noiseSel++;
   __bis_SR_register(GIE);       // enable general interrupt
@@ -78,7 +82,25 @@ void __attribute__ ((interrupt(TIMER0_A0_VECTOR))) Timer0_A0_ISR (void)
 #error Compiler not supported!
 #endif
 {
+
+    if ((__noiseSel & 0x01) == 0x00) {     // time the node is "offline"
+
+        ADC12IER0 &= ~ADC12IE0;  // disable ADC interrupt
+
+        TA0R = 0;   // reset timer
+        TA0CCR0 = rstInterval+__noise[__noiseSel];
+        __noiseSel++;
+
+        // sleep LPM0 -> SMCLOCK remains active if SMCLKOFF = 0 (if SMCLKOFF in use)
+        __bis_SR_register(LPM0_bits | GIE);
+        __no_operation();
+    }
+
+    else    // reset
+    {
     __bic_SR_register(GIE);       // disable general interrupt
     __no_operation();
     PMMCTL0 = PMMPW|PMMSWBOR;
+    }
 }
+
